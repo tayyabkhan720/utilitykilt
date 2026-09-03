d3b272b5756374ea89635488f1d30a08d3662a73
tayyabkhan456 <muhammad.tayyeb@esparksinc.com>
2026-09-03 23:51:16 +0500

    best seller product widget

---
+++ app/code/Emizentech/Bestsellerwidget/Block/Currentcat.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Emizentech\Bestsellerwidget\Block;
+
+class Currentcat extends \Magento\Framework\View\Element\Template
+{
+        protected $_registry;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    )
+    {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+
+    public function getCurrentCategory()
+    {
+        return $this->_registry->registry('current_category');
+    }
+
+
+}
+?>
